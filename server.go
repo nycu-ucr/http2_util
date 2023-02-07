9b8d41a6f1caf19986ea82e705b559cc5fb7363d
Shung <jonson111088@gmail.com>
2023-02-07 14:26:06 +0800

    Use our onvm handler

--- server.go
+++ server.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/nycu-ucr/gonet/http"
 	"github.com/nycu-ucr/net/http2"
-	"github.com/nycu-ucr/net/http2/h2c"
+	"github.com/nycu-ucr/net/http2/onvm2c"
 	"github.com/pkg/errors"
 )
 
@@ -23,14 +23,16 @@ func NewServer(bindAddr string, preMasterSecretLogPath string, handler http.Hand
 		return nil, errors.New("server needs handler to handle request")
 	}
 
-	h2Server := &http2.Server{
+	h2s := &http2.Server{
 		// TODO: extends the idle time after re-use openapi client
-		IdleTimeout: 1 * time.Millisecond,
+		// IdleTimeout: 1 * time.Millisecond,
+		IdleTimeout: 10 * time.Second,
 	}
 	server = &http.Server{
 		USING_ONVM_SOCKET: true, // Select which to use, ONVM or TCP
 		Addr:              bindAddr,
-		Handler:           h2c.NewHandler(handler, h2Server),
+		Handler:           onvm2c.NewHandler(handler, h2s),
+		// Handler:           h2c.NewHandler(handler, h2s),
 	}
 
 	if preMasterSecretLogPath != "" {
